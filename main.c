00d061c1e212b3036caf462dcb69106b04f03d82
bozhou94 <boy.zhou94@gmail.com>
2015-11-06 22:29:01 +0000

    got rid of halting

--- main.c
+++ main.c
@@ -6,22 +6,34 @@
 
 #include "portaudio.h"
 
+// Audio capture parameters
 #define SAMPLE_RATE 44100
 #define NUM_CHANNELS 1
-#define BUFFER_LENGTH 256
-#define SUCCESS 1
-#define FAILURE 0
+#define FRAMES_PER_BUFFER 1024
 
 PaStream *stream;
 PaError err;
 
+typedef struct {
+  int frameIndex;
+  int maxFrameIndex;
+  char* recordedSamples;
+} testData;
+
+// File parameters
+#define FILE_TTL 600 // Time before we switch to a new file in seconds
+
+time_t lastCheckpoint, newCheckpoint;
+FILE* dataFile; // holds the data accumulated until now
+FILE* nextFile;
+
+// Flow control
+#define SUCCESS 1
+#define FAILURE 0
+
 int looping; // Boolean to dictate whether or not we should contiue looping
 
-// For data collection purposes
-time_t lastCheckpoint;
-int sampleNumber;
-FILE* dataFile; // holds the data accumulated until now
-// FILE* gnuplotPipe; // Pipes commands to gnuplot
+char* getNextFileName();
 
 /**
  * Callback function for the stream
@@ -36,7 +48,17 @@ static int captureCallback(const void *inputBuffer, void *outputBuffer,
   if (inputBuffer == NULL) {
     return paContinue;
   }
-  
+
+  // Check if we should create a new checkpoint
+  time(&newCheckpoint);
+  double timeDiff = difftime(newCheckpoint,lastCheckpoint);
+  // printf("[DEBUG] Time difference is %f\n", timeDiff);
+
+  if ((int) timeDiff > FILE_TTL) {
+    lastCheckpoint = newCheckpoint;
+    dataFile = freopen(getNextFileName(), "wb", dataFile);
+  }
+
   // Loop through the input to obtain samples
   float* inData = (float*) inputBuffer;
   
@@ -57,20 +79,39 @@ static int captureCallback(const void *inputBuffer, void *outputBuffer,
  * Initializes PortAudio and an audio stream
  */
 int initCapture() {
+
+  PaStreamParameters inputParameters;
+  testData data;
+  int i;
+  int totalFrames;
+  
   err = Pa_Initialize();
   if( err != paNoError ) { 
     printf( "PortAudio error: %s\n", Pa_GetErrorText( err ) );
     return FAILURE; 
   };
+
+  // Set up stream
+  data.maxFrameIndex = totalFrames = SAMPLE_RATE;
+  data.frameIndex = 0;
+  data.recordedSamples = (char*)malloc(totalFrames * sizeof(char));
+  for(i = 0; i < totalFrames;i++) data.recordedSamples[i]= 0;
+
+  inputParameters.device = Pa_GetDefaultInputDevice();
+  inputParameters.channelCount = 1;
+  inputParameters.sampleFormat = paFloat32;
+  inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
+  inputParameters.hostApiSpecificStreamInfo = NULL;
+
   
-  err = Pa_OpenDefaultStream( &stream,
-			      NUM_CHANNELS, // stereo input
-			      NUM_CHANNELS, // stereo output
-			      paFloat32, // 32 bit floating point output
-			      SAMPLE_RATE,
-			      BUFFER_LENGTH, // frames per buffer
-			      captureCallback,
-			      NULL ); // pointer that will be passed to your callback
+  err = Pa_OpenStream( &stream,
+		       &inputParameters,
+		       NULL, 
+		       SAMPLE_RATE,
+		       FRAMES_PER_BUFFER,
+		       paClipOff,
+		       captureCallback,
+		       &data ); // pointer that will be passed to your callback
   
   if( err != paNoError ) { 
     printf( "PortAudio error: %s\n", Pa_GetErrorText( err ) );
@@ -138,28 +179,32 @@ char* getNextFileName() {
 }
 
 int main() {
+
   // Initialize structures
   time(&lastCheckpoint);
   dataFile = fopen(getNextFileName(), "wb");
-  // gnuplotPipe = popen("gnuplot -persistent", "w");	
-  // sampleNumber = 0;
+  err = paNoError;
 
   // Initialize and start the capture
   if (initCapture()) {
     if (startCapture()) {
       
       // Looping the capture
-      looping = SUCCESS;
-      while (looping) {}
+      // looping = SUCCESS;
+      while (1) {}
       
       // Stop and kill the capture
+      /*
       if (stopCapture()) {
 	if (killCapture()) {
+	  fclose(dataFile);
 	  printf("[INFO] Capture Finished\n");
 	  return SUCCESS;
 	}
-      }
+	}*/
     }
   }
+  fclose(dataFile);
+  //printf("[INFO] Capture Stalled\n");
   return FAILURE;
 }
